cfeb258046bcd094ee9ecc10568cb1525a9cfb06
Denny P Sam <itsdennian@Dennys-Mac.local>
2018-01-03 21:24:44 +0530

    Implemented function to remove unwanted characters and spaces

--- interpreter.go
+++ interpreter.go
@@ -6,13 +6,41 @@ import (
 	"os"
 )
 
-func main() {
+var words = [8]string{">", "<", "+", "-", ".", ",", "[", "]"}
 
+//eliminate characters not supported to get a single line string of instructions
+func minimiseCode(code string) string {
+	var minimised string
+	for _, ch := range code {
+		for _, word := range words {
+			if string(ch) == word {
+				minimised += string(ch)
+			}
+		}
+	}
+	return minimised
+}
+
+//Return the result of the program
+func giveOutput(code string) string {
+
+	for _, ch := range code {
+		switch string(ch) {
+		case "+":
+			fmt.Println(ch)
+		}
+	}
+	return code
+}
+
+func main() {
 	var code string
+	var minimised string
+	var output string
 
 	//Print welcome message
 	fmt.Println("An interpreter for the language Brainfuck \nVersion 1.0")
-	fmt.Println(">>>")
+	fmt.Print(">>>")
 
 	//Input buffer
 	input := bufio.NewScanner(os.Stdin)
@@ -22,11 +50,8 @@ func main() {
 		code += input.Text()
 	}
 
-	//eliminate spaces in the code to get a single line string of instructions
-	for _, ch := range code {
-		if string(ch) == " " {
-
-		}
-	}
+	minimised = minimiseCode(code)
+	output = giveOutput(minimised)
+	fmt.Println(output)
 
 }
